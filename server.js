7d740491c9c166c66c926c38dfe7ed002cf09339
Przemek Bielak <przemyslaw.bielak@protonmail.com>
2019-05-18 10:44:39 +0200

    add question schema

--- server.js
+++ server.js
@@ -2,18 +2,58 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const graphqlHTTP = require("express-graphql");
+const { buildSchema } = require("graphql");
 const fetch = require("node-fetch");
-const fs = require('fs');
-const schema = require("./schema/schema");
+const fs = require("fs");
 
 const app = express();
 const PORT = 8080;
+
+let stackData = {};
 
 app.use(bodyParser.json());
 app.use(
   "/graphql/",
   graphqlHTTP({
-    schema,
+    schema: buildSchema(`
+      type Owner {
+        reputation: Int
+        user_id: Int
+        user_type: String
+        profile_image: String
+        display_name: String
+        link: String
+      }
+
+      type Question {
+        tags: [String]
+        owner: Owner
+        is_answered: Boolean
+        view_count: Int
+        accepted_answer_id: Int
+        answer_count: Int
+        score: Int
+        last_activity_date: Int
+        creation_date: Int
+        last_edit_date: Int
+        question_id: Int
+        link: String
+        title: String
+      }
+
+      type RootQuery {
+        questions: [Question]
+      }
+
+      schema {
+        query: RootQuery
+      }
+    `),
+    rootValue: {
+      questions: () => {
+        return stackData;
+      }
+    },
     graphiql: true
   })
 );
@@ -25,21 +65,27 @@ app.get("/", (req, res) => {
 
 const questionSorts = ["activity", "votes", "hot", "week", "month"];
 
-let stackData = {};
+const getStackQuestions = questions => {
+  (async () => {
+    for (sort of questions) {
+      const res = await fetch(
+        `https://api.stackexchange.com/2.2/questions?order=desc&sort=${sort}&site=stackoverflow`
+      );
+      const data = await res.json();
 
-(async () => {
-  for (sort of questionSorts) {
-    const res = await fetch(
-      `https://api.stackexchange.com/2.2/questions?order=desc&sort=${sort}&site=stackoverflow`
+      stackData = data.items;
+    }
+
+    fs.writeFile(
+      "./stackdata.json",
+      JSON.stringify(stackData, null, 2),
+      err => {
+        console.log(err);
+      }
     );
-    const data = await res.json();
 
-    stackData[sort] = data.items;
-  }
+    app.listen(PORT, () => console.log(`Serving on port ${PORT}`));
+  })();
+};
 
-  fs.writeFile('./stackdata.json', JSON.stringify(stackData, null, 2), (err) => {
-    console.log(err);
-  })
-
-  // app.listen(PORT, () => console.log(`Serving on port ${PORT}`));
-})();
+getStackQuestions(questionSorts);
